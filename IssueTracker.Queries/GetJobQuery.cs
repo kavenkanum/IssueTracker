2dfb442208d37b0820236cde5147336f6c5b122a
Aneta <aneta.dabrowska@puredev.eu>
2020-03-09 09:51:49 +0100

    New Queries & QueryDbContext

---
+++ IssueTracker.Queries/GetJobQuery.cs
@@ -0,0 +1,51 @@
+﻿using IssueTracker.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Queries
+{
+    public class JobDto
+    {
+        public int JobId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AssignedUserID { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime DateOfCreate { get; set; }
+    }
+    public class GetJobQuery : IRequest<JobDto>
+    {
+        public GetJobQuery(int jobId)
+        {
+            JobId = jobId;
+        }
+        public int JobId { get; set; }
+    }
+
+    public class GetJobQueryHandler : IRequestHandler<GetJobQuery, JobDto>
+    {
+        private readonly QueryDbContext _queryDbContext;
+        public GetJobQueryHandler(QueryDbContext queryDbContext)
+        {
+            _queryDbContext = queryDbContext;
+        }
+        public Task<JobDto> Handle(GetJobQuery request, CancellationToken cancellationToken)
+        {
+            var job = _queryDbContext.Jobs.FirstOrDefault(j => j.Id == request.JobId);
+            return Task.FromResult(new JobDto()
+            {
+                JobId = job.Id,
+                Name = job.Name,
+                Description = job.Description,
+                AssignedUserID = job.AssignedUserId,
+                Deadline = job.Deadline,
+                DateOfCreate = job.DateOfCreate
+            });
+        }
+    }
+}
---
+++ IssueTracker.Queries/GetListOfProjectJobsQuery.cs
@@ -0,0 +1,51 @@
+﻿using CSharpFunctionalExtensions;
+using IssueTracker.Domain.Language;
+using IssueTracker.Domain.Repositories;
+using IssueTracker.Persistence;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Queries
+{
+    public class ProjectJobDto
+    {
+        public int JobId { get; set; }
+        public string Name { get; set; }
+        public string AssignedUserId { get; set; }
+        public Status Status { get; set; }
+        public DateTime Deadline { get; set; }
+    }
+    public class GetListOfProjectJobsQuery : IRequest<ICollection<ProjectJobDto>>
+    {
+        public GetListOfProjectJobsQuery(int projectId)
+        {
+            ProjectId = projectId;
+        }
+        public int ProjectId { get; set; }
+    }
+    public class GetListOfProjectJobsQueryHandler : IRequestHandler<GetListOfProjectJobsQuery, ICollection<ProjectJobDto>>
+    {
+        private readonly QueryDbContext _queryDbContext;
+        public GetListOfProjectJobsQueryHandler(QueryDbContext queryDbContext)
+        {
+            _queryDbContext = queryDbContext;
+        }
+
+        public Task<ICollection<ProjectJobDto>> Handle(GetListOfProjectJobsQuery request, CancellationToken cancellationToken)
+        {
+            var jobs = (_queryDbContext.Jobs.Where(j => j.ProjectId == request.ProjectId)
+                .Select(j => new ProjectJobDto()
+                {
+                    JobId = j.Id,
+                    Name = j.Name
+                })).ToList() as ICollection<ProjectJobDto>;
+            return Task.FromResult(jobs);
+        }
+    }
+
+}
