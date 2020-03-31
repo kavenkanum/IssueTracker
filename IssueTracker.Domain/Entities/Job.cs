7a7fd7af937832744c3312b7c25caefb22eec937
Aneta <aneta.dabrowska@puredev.eu>
2020-03-31 09:28:27 +0200

    Machine state & its implementation in Job methods

--- IssueTracker.Domain/Entities/Job.cs
+++ IssueTracker.Domain/Entities/Job.cs
@@ -1,6 +1,8 @@
 ﻿using CSharpFunctionalExtensions;
 using IssueTracker.Domain.Language;
 using IssueTracker.Domain.Language.ValueObjects;
+using Stateless;
+using Stateless.Graph;
 using System;
 using System.Collections.Generic;
 
@@ -8,12 +10,34 @@ namespace IssueTracker.Domain.Entities
 {
     public class Job
     {
-        private Job(string name, DateTime dateOfCreate, Status status)
+        private readonly StateMachine<Status, Trigger> _machine;
+
+        private Job(string name, DateTime dateOfCreate, Status status) : this()
         {
             Name = name;
             DateOfCreate = dateOfCreate;
             Status = status;
             Comments = new List<Comment>();
+        }
+        public Job()
+        {
+            //configuration of the state machine
+
+            _machine = new StateMachine<Status, Trigger>(() => Status, s => Status = s);
+
+            _machine.Configure(Status.New)
+                .PermitReentry(Trigger.ChangeDeadline)
+                .PermitReentry(Trigger.EditProperties)
+                .PermitReentry(Trigger.ChangeName)
+                .PermitReentry(Trigger.ChangeDescription)
+                .PermitReentry(Trigger.ChangeAssignedUser)
+                .PermitReentry(Trigger.ChangePriority)
+                .Permit(Trigger.StartJob, Status.InProgress);
+
+            _machine.Configure(Status.InProgress)
+                .PermitReentry(Trigger.ChangeDeadline)
+                .PermitReentry(Trigger.ChangePriority)
+                .Permit(Trigger.FinishJob, Status.Done);
         }
 
         public int Id { get; private set; }
@@ -28,19 +52,24 @@ namespace IssueTracker.Domain.Entities
         public List<Comment> Comments { get; set; }
         public int StartsAfterJobId { get; set; }
 
-        
         public static Result<Job> Create(string name, DateTime dateOfCreate)
         {
             return Result.Create(!string.IsNullOrEmpty(name), "Task name cannot be empty")
                 .OnSuccess(() => new Job(name, dateOfCreate, Status.New));
         }
 
-        public void EditProperties(string description, Deadline deadline, Guid assignedUserId, Priority priority)
+        public Result EditProperties(string description, Deadline deadline, Guid assignedUserId, Priority priority)
         {
+            if (!_machine.CanFire(Trigger.EditProperties))
+                return Result.Fail("Unable to edit properties in that state (In progress / Done).");
+
             Description = description;
             Deadline = deadline;
             AssignedUserId = assignedUserId;
             Priority = priority;
+
+            _machine.Fire(Trigger.EditProperties);
+            return Result.Ok();
         }
 
         public void AddComment(Comment comment)
@@ -48,31 +77,67 @@ namespace IssueTracker.Domain.Entities
             Comments.Add(comment);
         }
 
-        public void ChangeName(string newName)
+        public Result ChangeName(string newName)
         {
+            if (!_machine.CanFire(Trigger.ChangeName))
+                return Result.Fail("Unable to change name in that state (In progress / Done).");
             Name = newName;
+            _machine.Fire(Trigger.ChangeName);
+            return Result.Ok();
         }
 
-        public void ChangeStatus(Status newStatus)
+        public Result StartJob()
         {
-            Status = newStatus;
+            if (!_machine.CanFire(Trigger.StartJob))
+                return Result.Fail("Unable to start job");
+
+            _machine.Fire(Trigger.StartJob);
+            return Result.Ok();
+        }
+
+        public Result FinishJob()
+        {
+            if (!_machine.CanFire(Trigger.FinishJob))
+                return Result.Fail("Unable to finish job");
+
+            _machine.Fire(Trigger.FinishJob);
+            return Result.Ok();
         }
         public Result ChangeDeadline(Deadline newDeadline)
         {
+            if (!_machine.CanFire(Trigger.ChangeDeadline))
+                return Result.Fail("Unable to change deadline");
+
             Deadline = newDeadline;
+            _machine.Fire(Trigger.ChangeDeadline);
             return Result.Ok();
         }
-        public void ChangeDescription(string newDescription)
+        public Result ChangeDescription(string newDescription)
         {
+            if (!_machine.CanFire(Trigger.ChangeDescription))
+                return Result.Fail("Unable to change description in that state (In progress / Done)");
+
             Description = newDescription;
+            _machine.Fire(Trigger.ChangeDescription);
+            return Result.Ok();
         }
-        public void ChangeAssignedUser(Guid newAssignedUserId)
+        public Result ChangeAssignedUser(Guid newAssignedUserId)
         {
+            if (!_machine.CanFire(Trigger.ChangeAssignedUser))
+                return Result.Fail("Unable to change assigned user in that state (In progress / Done)");
+
             AssignedUserId = newAssignedUserId;
+            _machine.Fire(Trigger.ChangeAssignedUser);
+            return Result.Ok();
         }
-        public void ChangePriority(Priority newPriority)
+        public Result ChangePriority(Priority newPriority)
         {
+            if (!_machine.CanFire(Trigger.ChangePriority))
+                return Result.Fail("Unable to change assigned user in that state (Done)");
+
             Priority = newPriority;
+            _machine.Fire(Trigger.ChangePriority);
+            return Result.Ok();
         }
 
     }
