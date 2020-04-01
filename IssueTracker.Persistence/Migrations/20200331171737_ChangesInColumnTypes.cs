740632587e4e0706a6f343dd104dac1fc7e3fb50
Aneta <aneta.dabrowska@puredev.eu>
2020-04-01 11:32:11 +0200

    Bugfix: migration & dropping foreign & primary keys

---
+++ IssueTracker.Persistence/Migrations/20200331171737_ChangesInColumnTypes.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IssueTracker.Migrations
+{
+    public partial class ChangesInColumnTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey("FK_Jobs_Users_UserId", "Jobs");
+            migrationBuilder.DropPrimaryKey("PK_Users", "Users");
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Jobs",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Deadline",
+                table: "Jobs",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AssignedUserId",
+                table: "Jobs",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Comment",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Deadline",
+                table: "Jobs",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Jobs",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AssignedUserId",
+                table: "Jobs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldDefaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Comment",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(Guid));
+        }
+    }
+}
