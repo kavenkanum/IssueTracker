dc81e7e4a6aeb2196dd545f84263afa816893087
Aneta <aneta.dabrowska@puredev.eu>
2020-04-16 12:56:14 +0200

    Feature: access to logged user in whole app & transformation roles to permissions

---
+++ IssueTracker.Domain.Language/CurrentUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace IssueTracker.Domain.Language
+{
+    public class CurrentUser
+    {
+        public static IReadOnlyDictionary<string, ICollection<Permission>> dictionary = new Dictionary<string, ICollection<Permission>>()
+        {
+            { "admin", new List<Permission>()
+            {
+                Permission.DeleteJob,
+                Permission.AssignUsersToJob
+            }},
+            { "manager", new List<Permission>()
+            {
+                Permission.DeleteJob,
+                Permission.AssignUsersToJob,
+                Permission.AddJob
+            } }
+        };
+
+        private readonly ClaimsPrincipal _user;
+        private readonly IReadOnlyCollection<Permission> _permissions;
+
+        public CurrentUser(ClaimsPrincipal user)
+        {
+            _user = user;
+            _permissions = AssignPermissions(user);
+            Id = GetId(user);
+        }
+
+        public string Id { get; }
+        public bool HasPermission(Permission permission) => _permissions.Contains(permission);
+
+        private IReadOnlyCollection<Permission> AssignPermissions(ClaimsPrincipal user)
+        {
+            var roles = user.Claims.Where(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role").Select(c => c.Value);
+            return dictionary.Where(e => roles.Contains(e.Key)).SelectMany(kv => kv.Value).Distinct().ToList();
+        }
+
+        private string GetId(ClaimsPrincipal user)
+        {
+            return user.Claims.FirstOrDefault(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier").Value;
+        }
+    }
+}
