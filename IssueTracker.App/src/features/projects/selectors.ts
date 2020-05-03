9e16e0caa15a17add76368c0e4856539f58c9a69
Aneta <aneta.dabrowska@puredev.eu>
2020-05-03 10:24:44 +0200

    APP: compression redux files into one file: slice & thunk

--- IssueTracker.App/src/features/projects/selectors.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from "typesafe-actions";
-
-export const getProjectName = (state: RootState) => state.project.name;
-export const getProjectId = (state: RootState) => state.project.id;
