59bb2719a39c03441e8fce0a7962d310769785df
Aneta <aneta.dabrowska@puredev.eu>
2020-05-05 20:00:36 +0200

    API: changes in commands % repository to return id after adding new comment/job

--- IssueTracker.Domain/Repositories/IJobRepository.cs
+++ IssueTracker.Domain/Repositories/IJobRepository.cs
@@ -9,7 +9,7 @@ namespace IssueTracker.Domain.Repositories
     public interface IJobRepository
     {
         Task<Maybe<Job>> GetAsync(int jobId);
-        Task<Result> SaveAsync(Job job);
+        Task<Result<int>> SaveAsync(Job job);
         Result Delete(Job job);
     }
 }
