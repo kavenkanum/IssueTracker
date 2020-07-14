f0b820416ac8a68baea9ba953b830594d51aa0ba
Aneta <aneta.dabrowska@puredev.eu>
2020-07-14 13:01:44 +0200

    Extraction deadline (value obj) tests from Jobtests

---
+++ IssueTracker.UnitTests/ValueObjects/DeadlineTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using IssueTracker.Domain.Language.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IssueTracker.UnitTests.ValueObjects
+{
+    public class DeadlineTests
+    {
+        [Fact]
+        public void ShouldNotSetDeadline()
+        {
+            var currentDate = DateTime.Now;
+            var newDeadline = DateTime.Now.AddDays(-1);
+
+            var deadline = Deadline.CreateOptional(newDeadline, currentDate);
+
+            deadline.IsSuccess.Should().BeFalse();
+        }
+
+        [Fact]
+        public void ShouldNotSetDeadlineAtTheSameDay()
+        {
+            var currentDate = DateTime.Now;
+            var newDeadline = DateTime.Now;
+
+            var deadline = Deadline.CreateOptional(newDeadline, currentDate);
+
+            deadline.IsSuccess.Should().BeFalse();
+        }
+        [Fact]
+        public void ShouldSetDeadline()
+        {
+            var currentDate = DateTime.Now;
+            var newDeadline = DateTime.Now.AddDays(+1);
+
+            var deadline = Deadline.CreateOptional(newDeadline, currentDate);
+
+            deadline.IsSuccess.Should().BeTrue();
+        }
+    }
+}
